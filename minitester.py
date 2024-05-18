e940e2788884264dd4bdc3646869b5577d566e25
mcombeau <mcombeau@student.42.fr>
2024-05-18 11:49:45 +0200

    create input files; update output file checking

--- minitester.py
+++ minitester.py
@@ -10,8 +10,19 @@ import re
 bash_path = 'bash'
 minishell_path = os.path.abspath('../minishell')
 
-bash_dir = 'test_output/bash'
-mini_dir = 'test_output/minishell'
+test_dir = 'test_cwd'
+bash_dir = 'test_cwd/bash'
+mini_dir = 'test_cwd/minishell'
+
+test_files = [
+    'forbidden_dir',
+    'forbidden_file',
+    'valid_infile_1',
+    'valid_infile_2',
+    'executable_file',
+    'existing_dir',
+    'existing_file'
+]
 
 test_count = 0
 test_fail_count = 0
@@ -83,75 +94,122 @@ def compare_files(file1, file2):
         print(f'One or both files do not exist: {file1}, {file2}')
         return False
 
-def output_files_ok(bash_files, mini_files):
-    ok = True
+def output_files_ok():
+    all_files_ok = True
 
-    if len(bash_files) != len(mini_files):
+    bash_files_set = set()
+    mini_files_set = set()
+    
+    for root, dirs, files in os.walk(bash_dir):
+        for file in files:
+            bash_files_set.add(os.path.relpath(os.path.join(root, file), bash_dir))
+    
+    for root, dirs, files in os.walk(mini_dir):
+        for file in files:
+            mini_files_set.add(os.path.relpath(os.path.join(root, file), mini_dir))
+    
+    bash_files_set = {file for file in bash_files_set if os.path.basename(file) not in test_files}
+    mini_files_set = {file for file in mini_files_set if os.path.basename(file) not in test_files}
+    
+    if bash_files_set != mini_files_set:
         print('Bash and minishell have different output files!')
-        print(f'bash files: {bash_files}')
-        print(f'mini files: {mini_files}')
-        ok = False
+        print(f'bash files: {bash_files_set}')
+        print(f'mini files: {mini_files_set}')
+        all_files_ok = False
 
-    for file in bash_files:
-        bash_file_path = os.path.join(bash_dir, os.path.basename(file))
-        mini_file_path = os.path.join(mini_dir, os.path.basename(file))
-        if compare_files(bash_file_path, mini_file_path) == False:
-            ok = False
+    for file in bash_files_set:
+        bash_file_path = os.path.join(bash_dir, file)
+        mini_file_path = os.path.join(mini_dir, file)
+        if not compare_files(bash_file_path, mini_file_path):
+            all_files_ok = False
 
-    if ok == True:
+    if all_files_ok:
         print(f'\tOutput files... OK')
-
-    return ok
+    return all_files_ok
 
 def test_command(command):
     global test_count
     global test_fail_count
     global test_pass_count
     test_count += 1
+    expected_passed = 3
     print(f'{test_count}: [{command}]')
 
     bash_stdout, bash_stderr, bash_returncode = run_command(command, bash_path, bash_dir)
     mini_stdout, mini_stderr, mini_returncode = run_command(command, minishell_path, mini_dir)
-
-    bash_files = get_files_in_dir(bash_dir)
-    mini_files = get_files_in_dir(mini_dir)
 
     passed = 0
 
     passed += stdout_ok(bash_stdout, mini_stdout)
     passed += stderr_ok(bash_stderr, mini_stderr)
     passed += returncode_ok(bash_returncode, mini_returncode)
-    passed += output_files_ok(bash_files, mini_files)
+    if '>' in command or '>>' in command:
+        expected_passed = 4
+        passed += output_files_ok()
 
-    if passed == 4:
+    if passed == expected_passed:
         test_pass_count += 1
     else:
         test_fail_count += 1
 
 def setup_directories():
-    shutil.rmtree('test_output', ignore_errors=True)
-    os.makedirs('test_output/bash')
-    os.makedirs('test_output/minishell')
+    os.makedirs(bash_dir)
+    os.makedirs(mini_dir)
 
-def setup_test_files():
-    shutil.rmtree('test_input', ignore_errors=True)
-    os.makedirs('test_input')
-    # TODO: make forbidden dir
-    # TODO: make forbidden file
-    # TODO: make valid input file 1
-    # TODO: make valid input file 2
-    # TODO: make executable file
+def setup_test_files(path):
+    existing_dir = os.path.join(path, 'existing_dir')
+    os.makedirs(existing_dir, exist_ok=True)
+
+    forbidden_dir = os.path.join(path, 'forbidden_dir')
+    os.makedirs(forbidden_dir, exist_ok=True)
+    os.chmod(forbidden_dir, 0o000)
+
+    existing_file = os.path.join(path, 'existing_file')
+    with open(existing_file, 'w') as file:
+        file.write('This is a file that exists with r/w permissions')
+
+    forbidden_file = os.path.join(path, 'forbidden_file')
+    with open(forbidden_file, 'w') as file:
+        file.write('This is a forbidden file')
+    os.chmod(forbidden_file, 0o000)
+
+    valid_infile_1 = os.path.join(path, 'valid_infile_1')
+    with open(valid_infile_1, 'w') as file:
+        file.write('Take this kiss upon the brow!\nAnd, in parting from you now,\nThus much let me avow-\nYou are not wrong, who deem\nThat my days have been a dream;\nYet if hope has flown away\nIn a night, or in a day,\nIn a vision, or in none,\nIs it therefore the less gone?\nAll that we see or seem\nIs but a dream within a dream.\n\nI stand amid the roar\nOf a surf-tormented shore,\nAnd I hold within my hand\nGrains of the golden sand-\nHow few! yet how they creep\nThrough my fingers to the deep,\nWhile I weep- while I weep!\nO God! can I not grasp\nThem with a tighter clasp?\nO God! can I not save\nOne from the pitiless wave?\nIs all that we see or seem\nBut a dream within a dream?\n\nEdgar Allan Poe\nA Dream Within a Dream')
+
+    valid_infile_2 = os.path.join(path, 'valid_infile_2')
+    with open(valid_infile_2, 'w') as file:
+        head_output = subprocess.run(['head', '-n', '12'], input=subprocess.run(['man', 'bash'], stdout=subprocess.PIPE, text=True).stdout, text=True, stdout=subprocess.PIPE).stdout
+        file.write(head_output)
+
+    executable_file = os.path.join(path, 'executable_file')
+    with open(executable_file, 'w') as file:
+        file.write('#!/bin/bash\necho "This is an executable file."')
+    os.chmod(executable_file, 0o755)
 
 def cleanup_directories():
-    shutil.rmtree('test_output')
+    paths = [
+        os.path.join(bash_dir, 'forbidden_dir'),
+        os.path.join(bash_dir, 'forbidden_file'),
+        os.path.join(mini_dir, 'forbidden_dir'),
+        os.path.join(mini_dir, 'forbidden_file'),
+    ]
+
+    for path in paths:
+        if os.path.exists(path):
+            os.chmod(path, 0o666)
+
+    shutil.rmtree(test_dir)
 
 def main():
     global test_count
     global test_fail_count
     global test_pass_count
 
+    cleanup_directories()
     setup_directories()
-    setup_test_files()
+    setup_test_files(bash_dir)
+    setup_test_files(mini_dir)
 
     try:
         with open('minishell_tests.txt', 'r') as file:
